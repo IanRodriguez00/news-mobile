820dfe5e3bf9dc87184e915689bb29f2c29c9c06
IanRodriguez00 <girm.00@hotmail.com>
2024-02-02 01:19:18 -0600

    Cambiado color de titulos

--- src/navegacion/StackNavegacion.tsx
+++ src/navegacion/StackNavegacion.tsx
@@ -21,8 +21,8 @@ export const StackNavegacion = ()=> {
     <Stack.Navigator
     screenOptions={{
       headerStyle:{
-        elevation:0,
         shadowColor:'transparent',
+        backgroundColor:'#20B2AA',
       },
         cardStyle:{
             backgroundColor:'white',
